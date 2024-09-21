b718ce62cb7462ebf640e7fe84d2463bfde199de
Дмитрий Мехеда <114097534+Dmitriz1@users.noreply.github.com>
2024-09-21 08:54:34 +0500

    Фигурные скобочки переставлены

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Main{
+    public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         int price; // переменная обозначающая стоимость полёта
         int bonus; // переменная обозначающая бонусы
