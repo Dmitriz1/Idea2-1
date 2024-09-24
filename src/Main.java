c22268fb55d50302aa73e7c45dc9c12dd996824f
Дмитрий Мехеда <114097534+Dmitriz1@users.noreply.github.com>
2024-09-24 07:39:29 +0500

    made it easier

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,7 @@
-import java.util.Scanner;
-
-public class Main{
-    public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        int price; // переменная обозначающая стоимость полёта
-        int bonus; // переменная обозначающая бонусы
-        System.out.print("Введите стоимость полёта: ");
-        price = in.nextInt();
-
-        bonus = price/20;
+public class Main {
+    public static void main(String[] args) {
+        int price = 13_676;
+        int bonus = price / 20;
         System.out.println("Количество начисленных за полёт баллов: " + bonus);
     }
 }
